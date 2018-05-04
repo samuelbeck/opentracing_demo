e4ffb0bd963e80737154888c82d50688c4148ee9
Samuel Beck <samuel_jeremias.beck@daimler.com>
2018-05-04 10:46:39 +0200

    Changed implementation to get tracer configuration details from environment variables.

--- opentracing_demo/src/main/java/com/themanawaha/opentracing_demo/OpentracingDemoApplication.java
+++ opentracing_demo/src/main/java/com/themanawaha/opentracing_demo/OpentracingDemoApplication.java
@@ -1,7 +1,12 @@
 package com.themanawaha.opentracing_demo;
 
-import com.uber.jaeger.Configuration;
-import com.uber.jaeger.samplers.ProbabilisticSampler;
+
+import io.jaegertracing.Configuration;
+import io.jaegertracing.Tracer;
+import io.jaegertracing.reporters.InMemoryReporter;
+import io.jaegertracing.reporters.Reporter;
+import io.jaegertracing.samplers.ProbabilisticSampler;
+import io.jaegertracing.samplers.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +16,13 @@ public class OpentracingDemoApplication {
 
 	@Bean
 	public io.opentracing.Tracer jaegerTracer() {
-		return new Configuration("hello_service", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
-				new Configuration.ReporterConfiguration())
-				.getTracer();
+
+		Configuration config = Configuration.fromEnv();
+
+		System.out.println("Config_Host: " + config.getReporter().getSenderConfiguration().getAgentHost());
+		System.out.println("Config_Port: " + config.getReporter().getSenderConfiguration().getAgentPort());
+
+		return config.getTracer();
 	}
 
 	public static void main(String[] args) {
