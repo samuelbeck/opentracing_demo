d570fa01fde6c03f17d157772e616b273429dcc2
Samuel Beck <sammy_13@gmx.net>
2018-05-08 11:31:39 +0200

    Added necessary environment variable to docker-compose.yml file.

--- opentracing_demo/src/main/java/com/themanawaha/opentracing_demo/OpentracingDemoApplication.java
+++ opentracing_demo/src/main/java/com/themanawaha/opentracing_demo/OpentracingDemoApplication.java
@@ -1,12 +1,6 @@
 package com.themanawaha.opentracing_demo;
 
-
 import io.jaegertracing.Configuration;
-import io.jaegertracing.Tracer;
-import io.jaegertracing.reporters.InMemoryReporter;
-import io.jaegertracing.reporters.Reporter;
-import io.jaegertracing.samplers.ProbabilisticSampler;
-import io.jaegertracing.samplers.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +15,7 @@ public class OpentracingDemoApplication {
 
 		System.out.println("Config_Host: " + config.getReporter().getSenderConfiguration().getAgentHost());
 		System.out.println("Config_Port: " + config.getReporter().getSenderConfiguration().getAgentPort());
+		System.out.println("Sampler_Host_Port: " + config.getSampler().getManagerHostPort());
 
 		return config.getTracer();
 	}
